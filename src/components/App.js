5e9248cf567de1143d1f326a56bc9e412acc9e4b
Niel <mosesdaniel201798@gmail.com>
2021-12-02 14:43:12 +0100

    front end initialised

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,31 @@ import Main from './Main'
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3 () {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-ethereum browser detected. You should consider using metamask!')
+    }
+  }
+
+  async loadBlockchainData () {
+    const web3 = window.web3
+    //load the account from the blockchain
+    const accounts = await web3.eth.getAccounts()
+    this.setState({
+      account: accounts[0]
+    })
+  }
+
   constructor(props) {
     super(props)
     this.state = {
